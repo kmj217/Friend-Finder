055fbd2efc05af8e6416929c4d2585c157d44f64
KMJ217 <kmj217@hotmail.com>
2019-07-24 15:24:12 -0400

    mostly works

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friends = [
+  {
+    name: "John",
+    photo: "https://picsum.photos/200/300",
+    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+  },
+  {
+    name: "Ted",
+    photo: "https://picsum.photos/200/300",
+    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+  },
+  {
+    name: "Mila",
+    photo: "https://picsum.photos/200/300",
+    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+  }
+];
+
+module.exports = friends;
