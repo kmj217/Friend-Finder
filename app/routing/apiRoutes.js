fe037f0e686df85be1865a71efcfedee9ea95b77
KMJ217 <kmj217@hotmail.com>
2019-07-24 19:06:57 -0400

    created heroku

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,46 +1,34 @@
-var friends = require("../data/friends");
+var path = require('path');
+var friends = require('../data/friends.js')
+module.exports = function(app){
 
-module.exports = function(app) {
 
-  app.get("/api/friends", function(req, res) {
-    res.json(friends);
-  });
+    app.get('/api/friends', function(req, res){
+        console.log("response")
+        res.json(friends)
+    })
 
-  app.post("/api/friends", function(req, res) {
-
-    var bestMatch = {
-      name: "",
-      photo: "",
-      friendDifference: 100
-    };
-
-    var userData = req.body;
-    var userScores = userData.scores;
-
-    var totalDifference = 0;
-
-    for (var i = 0; i < friends.length; i++) {
-
-      console.log(friends[i].name);
-      totalDifference = 0;
-
-      for (var j = 0; j < friends[i].scores[j]; j++) {
-
-        totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
-
-        if (totalDifference <= bestMatch.friendDifference) {
-
-          bestMatch.name = friends[i].name;
-          bestMatch.photo = friends[i].photo;
-          bestMatch.friendDifference = totalDifference;
+    app.post('/api/friends', function(req, res){
+        var totalDifference;
+        var greatestTotalDifference = 41;
+        var match;
+        var currentUser = req.body
+        // loop through the array of friends objects
+        for(var i in friends){
+            totalDifference = 0;
+            // loop through the scores array within each object
+            for(var j in friends[i].scores){
+                // calculate total difference in scores
+                totalDifference += Math.abs(friends[i].scores[j] - currentUser.scores[j])
+            }
+            // save the greatest total difference / match before moving on to the next friend
+            if(totalDifference < greatestTotalDifference){
+             greatestTotalDifference = totalDifference;
+             match = friends[i]
+            }
         }
-      }
-    }
-
-    friends.push(userData);
-
-    res.json(bestMatch);
-
-  });
-
-};
+        // add the current user object to the friends array
+        friends.push(currentUser)
+        res.send([match, greatestTotalDifference])
+    })
+}
